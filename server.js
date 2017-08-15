9f20e986b6766b530c76298c8bf6fc4b490cfa3b
neeraj <neeraj92144@gmail.com>
2017-08-15 23:03:53 +0530

    change on port handling

--- server.js
+++ server.js
@@ -3,9 +3,20 @@ var express= require('express');
 
 // Create our app
 var app=express();
+// to access environment varialbe use (process.env)
+const POST= process.env.PORT || 2017;
+
+app.use( (req,res,next)=>{
+  console.log('PROTOCOL--->',req.headers['x-forwarded-proto'] );
+  if(req.headers['x-forwarded-proto'] === 'http'){
+    next();
+  }else{
+    res.redirect('http://'+req.hostname+req.url);
+  }
+});
 
 app.use(express.static('public'));
 
-app.listen( 2017, function(){
-  console.log("My first react Server is running on port : --> 2017");
+app.listen( POST , function(){
+  console.log("My first react Server is running on port : --> "+ POST);
 });
