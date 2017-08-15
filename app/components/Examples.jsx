5e9db3d5934a92266433b18adc46c7edf7ca264b
neeraj <neeraj92144@gmail.com>
2017-08-15 15:24:14 +0530

    Added new example line

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 var Examples = (props) =>{
   return(
       <div>
-         Examples!
+          Welcom to examples page further examples that are very crucial!  
       </div>
   )
 }
