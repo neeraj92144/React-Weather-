b24876f54bb5946da93aabbf425cf63a75b8d83b
neeraj <neeraj92144@gmail.com>
2017-08-16 06:40:26 +0530

    error solved

--- server.js
+++ server.js
@@ -3,10 +3,27 @@ var express= require('express');
 
 // Create our app
 var app=express();
+// to access environment varialbe use (process.env)
+const POST= process.env.PORT || 2017;
 
+app.use( function(req,res,next){
+  // console.log('HTTPS/HTTP',req.headers['x-forwarded-proto']);
+  // console.log('HOSTNAMEW',req.hostname)
+  // console.log('Url-->',req.url);
+  if(req.headers['x-forwarded-proto'] === 'http'){
+    next();
+  }else if(req.headers['x-forwarded-proto'] === undefined){
+    console.log('INSide UNdeni');
+    next();
+  }
+  else{
+    console.log('else');
+    res.redirect('http://'+req.hostname+req.url);
+  }
+});
 
 app.use(express.static('public'));
 
-app.listen( 2017 , function(){
-  console.log("My first react Server is running on port : --> 2017");
+app.listen( POST , function(){
+  console.log("My first react Server is running on port : --> "+POST);
 });
