4dee2beee629f0c4442844fb7d55aedb274d931e
neeraj <neeraj92144@gmail.com>
2017-08-16 06:14:15 +0530

    try

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 var Examples = (props) =>{
   return(
       <div>
-          Welcom to examples page further examples that are very crucial!  
+          Welcom to examples page further examples that are very crucial!
       </div>
   )
 }
